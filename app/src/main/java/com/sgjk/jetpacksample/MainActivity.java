4c5da5e6341448bc738ddc5c89d27ec53c6f3035
zhangfc <zhangfengcheng@shiguangjk.com>
2020-12-24 17:11:54 +0800

    project init

---
+++ app/src/main/java/com/sgjk/jetpacksample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sgjk.jetpacksample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
